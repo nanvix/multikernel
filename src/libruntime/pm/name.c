995df78010e1d2a89ed7381f7c6772252c614068
João Fellipe <felippe.uller@gmail.com>
2021-02-09 17:25:09 -0300

    [name] Address Local Solver v1.0

--- src/libruntime/pm/name.c
+++ src/libruntime/pm/name.c
@@ -27,6 +27,8 @@
 #define __NEED_NAME_SERVICE
 
 #include <nanvix/kernel/mailbox.h>
+#include <nanvix/sys/condvar.h>
+#include <nanvix/sys/mutex.h>
 #include <nanvix/sys/thread.h>
 #include <nanvix/servers/name.h>
 #include <nanvix/runtime/pm.h>
@@ -35,10 +37,12 @@
 #include <posix/errno.h>
 #include <posix/stdbool.h>
 
+#define DEBUG 0
+
 /**
  * @brief ID of name snooper thread.
  */
-static kthread_t nanvix_name_snooper_tid;
+static kthread_t nanvix_name_snooper_tid = -1;
 
 /**
  * @brief Mailbox for small messages.
@@ -61,6 +65,26 @@ PRIVATE struct {
 		.name = "",
 		.inbox_port_nr = -1
 	}
+};
+
+PRIVATE struct nanvix_mutex _name_lock;
+
+/**
+ * @brief Locking variables for address lookups.
+ */
+PRIVATE struct nanvix_mutex _local_lock;
+
+PRIVATE struct nanvix_cond_var _local_condvar;
+
+PRIVATE struct
+{
+	char name[NANVIX_PROC_NAME_MAX];
+	int port_nr;
+	int read;
+} _addr_lookup_result = {
+	.name    = "",
+	.port_nr = -1,
+	.read    =  1
 };
 
 /*============================================================================*
@@ -162,6 +186,7 @@ PRIVATE void _local_name_unregister(const char *name)
 int nanvix_name_lookup(const char *name)
 {
 	int ret;
+	int inbox;
 	struct name_message msg;
 
 	/* Initilize name client. */
@@ -176,11 +201,30 @@ int nanvix_name_lookup(const char *name)
 	message_header_build(&msg.header, NAME_LOOKUP);
 	ustrcpy(msg.op.lookup.name, name);
 
-	if ((ret = kmailbox_write(server, &msg, sizeof(struct name_message))) != sizeof(struct name_message))
-		return (ret);
+	uassert(nanvix_mutex_lock(&_name_lock) == 0);
 
-	if ((ret = kmailbox_read(stdinbox_get(), &msg, sizeof(struct name_message))) != sizeof(struct name_message))
+	if ((ret = kmailbox_write(server, &msg, sizeof(struct name_message))) != sizeof(struct name_message))
+	{
+		uassert(nanvix_mutex_unlock(&_name_lock) == 0);
 		return (ret);
+	}
+
+	inbox = stdinbox_get();
+
+	/**
+	 * @brief Sets the stdinbox remote to receive exclusively from the Name Server outbox.
+	 *
+	 * @todo Get further information about the necessity of a stronger message selection.
+	 */
+	uassert(kmailbox_set_remote(inbox, NAME_SERVER_NODE, MAILBOX_ANY_PORT) == 0);
+
+	if ((ret = kmailbox_read(inbox, &msg, sizeof(struct name_message))) != sizeof(struct name_message))
+	{
+		uassert(nanvix_mutex_unlock(&_name_lock) == 0);
+		return (ret);
+	}
+
+	uassert(nanvix_mutex_unlock(&_name_lock) == 0);
 
 	return (msg.op.ret.nodenum);
 }
@@ -195,6 +239,7 @@ int nanvix_name_lookup(const char *name)
 int nanvix_name_link(int nodenum, const char *name)
 {
 	int ret;
+	int inbox;
 	struct name_message msg;
 
 	/* Initilize name client. */
@@ -213,11 +258,30 @@ int nanvix_name_link(int nodenum, const char *name)
 	message_header_build(&msg.header, NAME_LINK);
 	ustrcpy(msg.op.link.name, name);
 
-	if ((ret = kmailbox_write(server, &msg, sizeof(struct name_message))) != sizeof(struct name_message))
-		return (ret);
+	uassert(nanvix_mutex_lock(&_name_lock) == 0);
 
-	if ((ret = kmailbox_read(stdinbox_get(), &msg, sizeof(struct name_message))) != sizeof(struct name_message))
+	if ((ret = kmailbox_write(server, &msg, sizeof(struct name_message))) != sizeof(struct name_message))
+	{
+		uassert(nanvix_mutex_unlock(&_name_lock) == 0);
 		return (ret);
+	}
+
+	inbox = stdinbox_get();
+
+	/**
+	 * @brief Sets the stdinbox remote to receive exclusively from the Name Server outbox.
+	 *
+	 * @todo Get further information about the necessity of a stronger message selection.
+	 */
+	uassert(kmailbox_set_remote(inbox, NAME_SERVER_NODE, MAILBOX_ANY_PORT) == 0);
+
+	if ((ret = kmailbox_read(inbox, &msg, sizeof(struct name_message))) != sizeof(struct name_message))
+	{
+		uassert(nanvix_mutex_unlock(&_name_lock) == 0);
+		return (ret);
+	}
+
+	uassert(nanvix_mutex_unlock(&_name_lock) == 0);
 
 	if (msg.header.opcode == NAME_SUCCESS)
 		return (0);
@@ -235,6 +299,7 @@ int nanvix_name_link(int nodenum, const char *name)
 int nanvix_name_unlink(const char *name)
 {
 	int ret;
+	int inbox;
 	struct name_message msg;
 
 	/* Initilize name client. */
@@ -249,11 +314,30 @@ int nanvix_name_unlink(const char *name)
 	message_header_build(&msg.header, NAME_UNLINK);
 	ustrcpy(msg.op.unlink.name, name);
 
-	if ((ret = kmailbox_write(server, &msg, sizeof(struct name_message))) != sizeof(struct name_message))
-		return (ret);
+	uassert(nanvix_mutex_lock(&_name_lock) == 0);
 
-	if ((ret = kmailbox_read(stdinbox_get(), &msg, sizeof(struct name_message))) != sizeof(struct name_message))
+	if ((ret = kmailbox_write(server, &msg, sizeof(struct name_message))) != sizeof(struct name_message))
+	{
+		uassert(nanvix_mutex_unlock(&_name_lock) == 0);
 		return (ret);
+	}
+
+	inbox = stdinbox_get();
+
+	/**
+	 * @brief Sets the stdinbox remote to receive exclusively from the Name Server outbox.
+	 *
+	 * @todo Get further information about the necessity of a stronger message selection.
+	 */
+	uassert(kmailbox_set_remote(inbox, NAME_SERVER_NODE, MAILBOX_ANY_PORT) == 0);
+
+	if ((ret = kmailbox_read(inbox, &msg, sizeof(struct name_message))) != sizeof(struct name_message))
+	{
+		uassert(nanvix_mutex_unlock(&_name_lock) == 0);
+		return (ret);
+	}
+
+	uassert(nanvix_mutex_unlock(&_name_lock) == 0);
 
 	if (msg.header.opcode == NAME_SUCCESS)
 		return (0);
@@ -323,7 +407,9 @@ PUBLIC int nanvix_name_address_lookup(const char *name, int *port)
 	/* @p name is a local process. */
 	if (nodenum == knode_get_num())
 	{
-		if ((*port = _local_address_lookup(name)) < 0)
+		*port = _local_address_lookup(name);
+
+		if ((*port) < 0)
 			ret = (-ENOENT);
 
 		return (ret);
@@ -332,31 +418,67 @@ PUBLIC int nanvix_name_address_lookup(const char *name, int *port)
 	/**
 	 * Resolves @p name address consulting the remote cluster associated.
 	 *
-	 * @note Since IOClusters currently does not support a local name daemon to
+	 * @note Since IOClusters does not support a local name daemon to
 	 * attend name resolution requests, this call may result in starvation for the
 	 * current thread. Make sure about the cluster associated with @p name is
 	 * a valid Compute Cluster.
+	 *
+	 * @todo Insert a security check to remove from the user the necessity of
+	 * ensure the remote clusternum.
 	 */
 
-	/* Opens an outbox to the remote name client. */
-	if ((outbox = kmailbox_open(nodenum, NANVIX_NAME_SNOOPER_PORT_NUM)) < 0)
-		return (outbox);
+	/* Locks the port placeholder during an address lookup inquirement. */
+	nanvix_mutex_lock(&_local_lock);
 
-	/* Build operation header. */
-	message_header_build(&msg.header, NAME_LOOKUP);
-	ustrcpy(msg.op.lookup.name, name);
+		/* Opens an outbox to the local name client. */
+		if ((outbox = kmailbox_open(nodenum, NANVIX_NAME_SNOOPER_PORT_NUM)) < 0)
+		{
+			ret = outbox;
+			goto end;
+		}
 
-	if ((ret = kmailbox_write(outbox, &msg, sizeof(struct name_message))) != sizeof(struct name_message))
-		goto end;
+		/* Build operation header. */
+		message_header_build(&msg.header, NAME_ADDR);
+		ustrcpy(msg.op.lookup.name, name);
 
-	if ((ret = kmailbox_read(stdinbox_get(), &msg, sizeof(struct name_message))) != sizeof(struct name_message))
-		goto end;
+		/* Notifies the local name daemon about the address_lookup to be done. */
+		if ((ret = kmailbox_write(outbox, &msg, sizeof(struct name_message))) != sizeof(struct name_message))
+			goto end;
 
-	/* Retrieves the port number returned by the remote client. */
-	*port = msg.op.ret.nodenum;
-	ret = nodenum;
+again:
+		/* Waits for the name snooper to sinalize that the remote lookup is done. */
+		uassert(nanvix_cond_wait(&_local_condvar, &_local_lock) == 0);
+
+		/* Not the expected process that was signaled? */
+		if (ustrcmp(_addr_lookup_result.name, name) != 0)
+		{
+			if (ustrcmp(_addr_lookup_result.name, "") == 0)
+			{
+				ret = (-EAGAIN);
+				goto end;
+			}
+
+			/* Wakes the next thread. */
+			nanvix_cond_signal(&_local_condvar);
+			goto again;
+		}
+
+		/* Retrieves the port number returned by the remote client. */
+		*port = _addr_lookup_result.port_nr;
+		ret = nodenum;
+
+		/* Remote lookup was successful? */
+		if ((*port) < 0)
+			ret = (-ENOENT);
+
+		/* Resets the shared variable. */
+		_addr_lookup_result.port_nr = -1;
+		ustrcpy(_addr_lookup_result.name, "");
+		_addr_lookup_result.read    = 1;
 
 end:
+	nanvix_mutex_unlock(&_local_lock);
+
 	uassert(kmailbox_close(outbox) == 0);
 
 	return (ret);
@@ -393,13 +515,18 @@ PUBLIC int nanvix_name_register(const char *name, int port_nr)
 	if ((ret = nanvix_name_link(knode_get_num(), name)) != 0)
 		return (ret);
 
-	/**
-	 * Registers @p name in the local translation table.
-	 *
-	 * @note If an error occurs, unlink @p name in the remote server.
-	 */
-	if ((ret = _local_name_register(port_nr, name)) != 0)
-		uassert(nanvix_name_unlink(name) == 0);
+	/* Locks the runtime to avoid data overwriting. */
+	uassert(nanvix_mutex_lock(&_local_lock) == 0);
+
+		/**
+		 * Registers @p name in the local translation table.
+		 *
+		 * @note If an error occurs, unlink @p name in the remote server.
+		 */
+		if ((ret = _local_name_register(port_nr, name)) != 0)
+			uassert(nanvix_name_unlink(name) == 0);
+
+	uassert(nanvix_mutex_unlock(&_local_lock) == 0);
 
 	return (ret);
 }
@@ -433,8 +560,12 @@ PUBLIC int nanvix_name_unregister(const char *name)
 	if ((ret = nanvix_name_unlink(name)) < 0)
 		return (ret);
 
+	uassert(nanvix_mutex_lock(&_local_lock) == 0);
+
 	/* Removes @p name from the local resolution table. */
 	_local_name_unregister(name);
+
+	uassert(nanvix_mutex_unlock(&_local_lock) == 0);
 
 	return (0);
 }
@@ -467,6 +598,18 @@ int nanvix_name_shutdown(void)
 /*============================================================================*
  * nanvix_name_snooper()                                                      *
  *============================================================================*/
+
+PRIVATE int _do_name_query(struct name_message *req)
+{
+	UNUSED(req);
+	return (0);
+}
+
+PRIVATE int _do_attend_lookup(struct name_message *req)
+{
+	UNUSED(req);
+	return (0);
+}
 
 /**
  * @brief Name resolution local snooper.
@@ -504,43 +647,99 @@ static void * nanvix_name_snooper(void *args)
 			) == sizeof(struct name_message)
 		);
 
+		/* Checks if requisition is a local query or a remote lookup. */
+		switch (request.header.opcode)
+		{
+			case NAME_SUCCESS:
+#if DEBUG
+		uprintf("[nanvix][name] name resolution answer received");
+#endif /* DEBUG */
+		again1:
+				uassert(nanvix_mutex_lock(&_local_lock) == 0);
+
+					/* Ensure that the last lookup was already consumed. */
+					if (!_addr_lookup_result.read)
+					{
+						nanvix_mutex_unlock(&_local_lock);
+						goto again1;
+					}
+
+					_addr_lookup_result.port_nr = request.op.addr_ans.port_nr;
+					ustrcpy(_addr_lookup_result.name, request.op.addr_ans.name);
+					_addr_lookup_result.read = 0;
+
+				uassert(nanvix_mutex_unlock(&_local_lock) == 0);
+
+				/* Wakes the requester up. */
+				uassert(nanvix_cond_signal(&_local_condvar) == 0);
+
+				break;
+
+			case NAME_FAIL:
+#if DEBUG
+		uprintf("[nanvix][name] name resolution answer failed");
+#endif /* DEBUG */
+				uassert(nanvix_mutex_lock(&_local_lock) == 0);
+
+					_addr_lookup_result.port_nr = -1;
+					ustrcpy(_addr_lookup_result.name, "");
+					_addr_lookup_result.read = 0;
+
+				uassert(nanvix_mutex_unlock(&_local_lock) == 0);
+
+				/* Wakes the requester up. */
+				uassert(nanvix_cond_signal(&_local_condvar) == 0);
+
+				break;
+
+			case NAME_ADDR:
+#if DEBUG
 		uprintf("[nanvix][name] resolution requisition received");
+#endif /* DEBUG */
 
-		name = request.op.lookup.name;
+				/* Opens output mailbox to the communicant peer. */
+				uassert((
+					outbox = kmailbox_open(
+						request.header.source,
+						NANVIX_NAME_SNOOPER_PORT_NUM
+					)) >= 0
+				);
 
-		/* Lookup to the local resolution table to find @p name address. */
-		port_nr = _local_address_lookup(name);
+				name = request.op.lookup.name;
 
-		response.op.ret.nodenum = port_nr;
-		response.op.ret.errcode = 0;
+				/* Lookup to the local resolution table to find @p name address. */
+				port_nr = _local_address_lookup(name);
 
-		/* Builds the response header. */
-		message_header_build(
-			&response.header,
-			(port_nr < 0) ? NAME_FAIL : NAME_SUCCESS
-		);
+				/* Builds the response header. */
+				message_header_build(
+					&response.header,
+					(port_nr < 0) ? NAME_FAIL : NAME_SUCCESS
+				);
 
-		/* Opens an outbox to the inbox of the requester. */
-		uassert((
-			outbox = kmailbox_open(
-				request.header.source,
-				request.header.mailbox_port
-			)) >= 0
-		);
+				response.op.addr_ans.port_nr = port_nr;
+				ustrcpy(response.op.addr_ans.name, name);
 
-		/* Sends the response back to the requisitant client. */
-		uassert(
-			kmailbox_write(
-				outbox,
-				&response,
-				sizeof(struct name_message)
-			) == sizeof(struct name_message)
-		);
+				/* Sends the response back to the requisitant client. */
+				uassert(
+					kmailbox_write(
+						outbox,
+						&response,
+						sizeof(struct name_message)
+					) == sizeof(struct name_message)
+				);
 
-		/* Closes the opened outbox. */
-		uassert(kmailbox_close(outbox) == 0);
+				/* Closes the opened outbox. */
+				uassert(kmailbox_close(outbox) == 0);
 
+				break;
+
+			default:
+				UNREACHABLE();
+		}
+
+#if DEBUG
 		uprintf("[nanvix][name] resolution requisition attended");
+#endif
 	}
 
 	return (NULL);
@@ -563,19 +762,39 @@ int __nanvix_name_setup(void)
 	if ((server = kmailbox_open(NAME_SERVER_NODE, NAME_SERVER_PORT_NUM)) < 0)
 		return (-1);
 
-	/**
-	 * Initializes the local name daemon.
-	 *
-	 * @note For now the daemon is only initialized in the Compute Clusters.
-	 * Create new threads in IO Clusters (Spawners) does not seem trivial and
-	 * need further information about the viability/necessity.
-	 *
-	 * @todo Discuss about this selective initialization.
-	 */
-	if (cluster_get_num() >= SPAWNERS_NUM)
-		uassert(kthread_create(&nanvix_name_snooper_tid, &nanvix_name_snooper, NULL) == 0);
+	uassert(nanvix_mutex_init(&_name_lock) == 0);
 
 	initialized = true;
+
+	return (0);
+}
+
+/*============================================================================*
+ * __nanvix_name_daemon_init()                                                *
+ *============================================================================*/
+
+/**
+ * @todo TODO: provide a detailed description for this function.
+ */
+int __nanvix_name_daemon_init(void)
+{
+	/* Nothing to do. */
+	if (nanvix_name_snooper_tid >= 0)
+		return (0);
+
+	/**
+	 * @brief Initialize local locking structures.
+	 *
+	 * @note These structures are initialized here because only cclusters can
+	 * emmit address_lookups and only in these cases these structures may become
+	 * necessary.
+	 */
+	uassert(nanvix_mutex_init(&_local_lock) == 0);
+
+	uassert(nanvix_cond_init(&_local_condvar) == 0);
+
+	/* Creates the name snooper thread. */
+	uassert(kthread_create(&nanvix_name_snooper_tid, &nanvix_name_snooper, NULL) == 0);
 
 	return (0);
 }
