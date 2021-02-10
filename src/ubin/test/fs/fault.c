fb075fce6c0222502785b7b075f1bc483890b882
lrr68 <luccaaugusto@protonmail.ch>
2021-02-10 15:34:47 -0300

    Improving create and create's tests

--- src/ubin/test/fs/fault.c
+++ src/ubin/test/fs/fault.c
@@ -149,8 +149,8 @@ static void test_fault_nanvix_vfs_unlink_invalid(void)
 	uassert(nanvix_vfs_unlink("/") == -EINVAL);
 	uassert(nanvix_vfs_unlink("some_file") == -ENOENT);
 
-	uassert((fd1 = nanvix_vfs_open(filename, (O_CREAT | O_RDONLY))) >= 0);
-	uassert((fd2 = nanvix_vfs_open(filename, (O_CREAT | O_RDONLY))) >= 0);
+	uassert((fd1 = nanvix_vfs_open(filename, (O_CREAT | O_RDONLY), (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH | S_IFREG))) >= 0);
+	uassert((fd2 = nanvix_vfs_open(filename, (O_CREAT | O_RDONLY), (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH | S_IFREG))) >= 0);
 	uassert(nanvix_vfs_unlink(filename) == -EBUSY);
 	uassert(nanvix_vfs_close(fd1) == 0);
 	uassert(nanvix_vfs_close(fd2) == 0);
